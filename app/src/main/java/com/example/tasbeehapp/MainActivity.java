433e239d8036f545d53db48ccd2d1912676e4249
Ahmad-Awais034 <bitf19a034@pucit.edu.pk>
2023-02-11 16:39:02 +0500

    add tasbeeh button in ui

--- app/src/main/java/com/example/tasbeehapp/MainActivity.java
+++ app/src/main/java/com/example/tasbeehapp/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.tasbeehapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int count=1;
+    TextView text;
+    AlertDialog.Builder alert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int i;
+        alert = new AlertDialog.Builder(this);
+        text=findViewById(R.id.counter);
+
+    }
+
+    public void increment(View view) {
+        text.setText(" "+count);
+        count++;
+
+    }
+
+    public void addTasbeeh(View view) {
+        LayoutInflater inflater=this.getLayoutInflater();
+        View inflaterView =inflater.inflate(R.layout.add_tasbeeh_layout,null);
+        alert.setView(inflaterView);
+        alert.show();
     }
 }
